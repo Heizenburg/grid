7f9ed98e3b08e495b0d61b6f023c27a8bdf2d433
Thato Sello <thatokagisosello@gmail.com>
2022-01-25 06:15:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function getAdjacent(matrix, i, j, k) {
+  const directions = [
+    [i - 1, j - 1],
+    [i - 1, j],
+    [i - 1, j + 1],
+    [i, j - 1],
+    [i, j + 1],
+    [i + 1, j - 1],
+    [i + 1, j],
+    [i + 1, j + 1],
+  ];
+  const [row, col] = directions[k];
+  // Check for last rows and columns
+  if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[i].length) {
+    return undefined;
+  }
+  return matrix[row][col];
+}
+
+function run(){
+  const matrix = [
+    [1, 2, 1],
+    [2, 1, 1],
+    [1, 1, 1]
+  ];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let sum = 0;
+      for (let k = 0; k < 8; k++) {
+        const res = getAdjacent(matrix, i, j, k);
+        console.log(i, j, k, res); // Do whatever you want here
+      }
+    }
+  }
+}
+
+run();
